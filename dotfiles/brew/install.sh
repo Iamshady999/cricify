f7effb4f3fcb18adf2f949b627defe61be3039d0
asheeshcric <ashiz2013@gmail.com>
2023-04-27 00:06:54 -0500

    Add brew installation command on top

--- dotfiles/brew/install.sh
+++ dotfiles/brew/install.sh
@@ -1,3 +1,7 @@
-echo "===Installing all the brew packages==="
+echo "===Installing brew==="
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+
+echo "===Installing all my brew packages==="
 brew install $(cat ~/dotfiles/brew/brew-packages.txt)
 
