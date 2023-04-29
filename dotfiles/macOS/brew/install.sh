358566fbafedf299acce075b4584ae47e5852062
asheeshcric <ashiz2013@gmail.com>
2023-04-29 18:24:16 -0500

    Update brew script to add it to PATH after installation

--- dotfiles/macOS/brew/install.sh
+++ dotfiles/macOS/brew/install.sh
@@ -3,6 +3,9 @@ SCRIPT_DIR="$(dirname "$0")"
 echo "===Installing brew==="
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+# Add brew to path
+echo "===Adding brew to path==="
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 echo "===Installing all my brew packages==="
 
