2f7a3b8f1cc35312d92f45542e2efac013d920f6
asheeshcric <ashiz2013@gmail.com>
2023-04-28 23:46:40 -0500

    Add zsh plugins installation script

---
+++ dotfiles/macOS/zsh/plugins.sh
@@ -0,0 +1,20 @@
+# Install zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Install zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Install zsh-history-substring-search
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+
+# Install sysadmin-util
+git clone https://github.com/skx/sysadmin-util.git ~/.oh-my-zsh/custom/plugins/sysadmin-util
+
+
+# echo "Add all of these to the plugins tuple in .zshrc"
+# echo "extract"
+# echo "zsh-autosuggestions"
+# echo "zsh-syntax-highlighting"
+# echo "autojump"
+# echo "zsh-history-substring-search"
+# echo "sysadmin-util"
