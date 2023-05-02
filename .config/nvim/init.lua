5d3ec2921406f9e5e679ab14e5845a195fd97156
asheeshcric <ashiz2013@gmail.com>
2023-05-01 22:56:09 -0500

    Add nvim config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,6 @@
+require("asheesh.plugins-setup")
+require("asheesh.core.options")
+require("asheesh.core.keymaps")
+require("asheesh.core.colorscheme")
+require("asheesh.plugins.comment")
+require("asheesh.plugins.nvim-tree")
