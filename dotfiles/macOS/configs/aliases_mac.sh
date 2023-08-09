ce23d53eb51ef8c334060caf5062a1029e3e4867
asheeshcric <ashiz2013@gmail.com>
2023-08-09 12:59:35 -0500

    Add a better alias for shutdown in macOS

--- dotfiles/macOS/configs/aliases_mac.sh
+++ dotfiles/macOS/configs/aliases_mac.sh
@@ -1,10 +1,11 @@
 # dotfiles config alias
-alias dotfiles='git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
+alias dotfiles="git --git-dir=$HOME/dotfiles/ --work-tree=$HOME"
 
 # Basics
-alias update='brew update'
-alias upgrade='brew upgrade'
-alias fuckoff='sudo shutdown -h now'
+alias update="sbrew update"s
+alias upgrade="sbrew upgrade"s
+# alias fuckoff='sudo shutdown -h now'
+alias fuckoff="osascript -e \'tell app \"System Events\" to shut down\'"
 
 # Python aliases
 alias python=python3
@@ -12,14 +13,15 @@ alias pip=pip3
 
 
 # Custom Aliases for Heracleia Server Connection
-HERACLEIA_DOMAIN="heracleia-master.uta.edu"
+# HERACLEIA_DOMAIN="heracleia-master.uta.edu"
+HERACLEIA_DOMAIN="heracleia.dhost.uta.edu"
 alias ssh-hades="ssh -t -p 297 ashish@$HERACLEIA_DOMAIN"
 alias ssh-poseidon="ssh -p 303 ashish@$HERACLEIA_DOMAIN"
 alias ssh-zeus="ssh -p 300 ashish@$HERACLEIA_DOMAIN"
 alias ssh-office='ssh asheesh@cric.dhost.uta.edu'
 alias ssh-office-vnc='ssh -L 5901:localhost:5901 -N -f asheesh@cric.dhost.uta.edu'
 #alias uta-vpn='sudo openconnect --protocol=pulse https://pulse-vpn.uta.edu'
-alias uta-vpn='source /home/asheesh/Documents/Github/personal-preferences/dotfiles/commands/uta_vpn.sh'
+# alias uta-vpn='source /home/asheesh/Documents/Github/personal-preferences/dotfiles/commands/uta_vpn.sh'
 alias jup-heracleia="ssh -N -L 9000:127.0.0.1:9000 -t -p 300 ashish@$HERACLEIA_DOMAIN"
 alias ldap-heracleia="sudo ssh -p 297 -f -L 80:127.0.0.1:80 -N ashish@$HERACLEIA_DOMAIN"
 
